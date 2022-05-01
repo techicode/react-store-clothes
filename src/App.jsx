d7dbdc5ddf82cdeac2cb2b603ba47681f56e390a
techicode <techicode@outlook.com>
2022-05-01 10:08:21 -0400

    added firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>Im shop page</h1>;
@@ -23,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import './navigation.styles.scss';
+
+import CrownLogo from '../../assets/crown.svg?component';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
